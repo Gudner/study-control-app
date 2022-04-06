06361f42ecd86660e60e2f137070da3692fa1a0b
Dmitry Shchukin <dmitry@speedymind.net>
2022-04-06 18:24:05 +0300

    MM-26 Создать на бэке запрос на удаление карточки предмета

--- Backend/Backend/EndpointDefinitions/SubjectCardEndpointDefinition.cs
+++ Backend/Backend/EndpointDefinitions/SubjectCardEndpointDefinition.cs
@@ -13,6 +13,20 @@ public class SubjectCardEndpointDefinition : IEndpointDefinition
         this.app = app;
         this.app.MapGet("/api/subjectcards", async (StudyControlDbContext dbContext) => Results.Json(await GetAllItems(dbContext))).RequireCors("allowAny");
         this.app.MapPut("/api/subjectcards", async (SubjectCardRequestBody body, StudyControlDbContext dbContext) => await AddNewItem(body, dbContext)).RequireCors("allowAny");
+        this.app.MapDelete("/api/subjectcards/{id}", async (int id, StudyControlDbContext dbContext) =>
+        {
+            var subjectCard = await dbContext.SubjectCards.SingleOrDefaultAsync(x => x.SubjectCardId == id);
+
+            if(subjectCard is null)
+            {
+                return Results.NotFound();
+            }
+
+            dbContext.SubjectCards.Remove(subjectCard);
+            await dbContext.SaveChangesAsync();
+
+            return Results.Ok();
+        }).RequireCors("allowAny");
     }
 
     public void DefineServices(IServiceCollection services) {}
