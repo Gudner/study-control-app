57b972cc8e5ee619b1830daca795b1e6ef62fb64
Delxys <baus753@yandex.ru>
2022-04-25 13:40:52 +0300

    SubjectContent delete method added

--- Frontend/study-control-app/components/subjectContent/SubjectContent.jsx
+++ Frontend/study-control-app/components/subjectContent/SubjectContent.jsx
@@ -10,11 +10,32 @@ import AddControlModal from "../addControlModal/AddControlModal";
 import AddTasckModal from "../addTasckModal/AddTasckModal";
 
 export default observer(function SubjectContent(props) {
+  console.log("props", props);
   console.log("propsDataControl", props.data);
-
+  let data = props.data;
   const [controlItem, setControlItem] = useState(props.data); //установка состояния
 
   //функция удаления элемента и обнавления стейта
+//регистрация формы
+const updatingDataServer = async (id, newAllData) => {
+  const res = await fetch(
+      `https://backend.revenant-games.online/api/controltasks/${id}`,
+      {
+          method: "DELETE",
+      }
+  );
+  const respons = await res;
+  if (respons.ok) {
+      props.setSubject(newAllData);
+  }
+};
+const removeTask = () => {
+  let newAllData = data.filter(
+      (controlTasks) =>
+          Number(controlTasks.controlTaskId) != Number(controlTasks.controlTaskId)
+  );
+  updatingDataServer(data.controlTaskId, newAllData);
+};
 
   return (
     <div className={style.container}>
@@ -58,6 +79,7 @@ export default observer(function SubjectContent(props) {
                       alt="del-icon"
                       width="24"
                       height="24"
+                      onClick={removeTask}
                     />
                   </div>
                 </div>
