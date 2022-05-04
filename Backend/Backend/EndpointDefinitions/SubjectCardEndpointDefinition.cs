0590b79aafedec5b9f1f0ac0cbd99726c0d5b68e
Dmitry Shchukin <dmitry@speedymind.net>
2022-05-04 20:39:46 +0300

    fix: invalid endpoints

--- Backend/Backend/EndpointDefinitions/SubjectCardEndpointDefinition.cs
+++ Backend/Backend/EndpointDefinitions/SubjectCardEndpointDefinition.cs
@@ -12,9 +12,9 @@ public class SubjectCardEndpointDefinition : IEndpointDefinition
     public void DefineEndpoints(WebApplication app)
     {
         this.app = app;
-        this.app.MapGet("api/subjectcards", 
+        this.app.MapGet("api/subjectcards/{id}", 
             async (int id, StudyControlDbContext dbContext) => await GetItem(dbContext, id)).RequireCors("allowAny");
-        this.app.MapGet("api/subjectcards/{id}",
+        this.app.MapGet("api/subjectcards",
             async (StudyControlDbContext dbContext) => Results.Json(await GetAllItems(dbContext), new JsonSerializerOptions(JsonSerializerDefaults.Web) { ReferenceHandler = ReferenceHandler.IgnoreCycles })).RequireCors("allowAny");
         this.app.MapPut("api/subjectcards", async (SubjectCardRequestBody body, StudyControlDbContext dbContext) => await AddNewItem(body, dbContext)).RequireCors("allowAny");
         this.app.MapDelete("api/subjectcards/{id}", async (int id, StudyControlDbContext dbContext) =>
