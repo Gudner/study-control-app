2ddbadefa868e3323a2c0c75be9a95134e87bbd5
Dmitry Shchukin <dmitry@speedymind.net>
2022-05-04 20:30:37 +0300

    update subject card api

--- Backend/Backend/EndpointDefinitions/SubjectCardEndpointDefinition.cs
+++ Backend/Backend/EndpointDefinitions/SubjectCardEndpointDefinition.cs
@@ -13,7 +13,9 @@ public class SubjectCardEndpointDefinition : IEndpointDefinition
     {
         this.app = app;
         this.app.MapGet("api/subjectcards", 
-            async (StudyControlDbContext dbContext) => Results.Json(await GetAllItems(dbContext), new JsonSerializerOptions(JsonSerializerDefaults.Web) { ReferenceHandler = ReferenceHandler.IgnoreCycles})).RequireCors("allowAny");
+            async (int id, StudyControlDbContext dbContext) => await GetItem(dbContext, id)).RequireCors("allowAny");
+        this.app.MapGet("api/subjectcards/{id}",
+            async (StudyControlDbContext dbContext) => Results.Json(await GetAllItems(dbContext), new JsonSerializerOptions(JsonSerializerDefaults.Web) { ReferenceHandler = ReferenceHandler.IgnoreCycles })).RequireCors("allowAny");
         this.app.MapPut("api/subjectcards", async (SubjectCardRequestBody body, StudyControlDbContext dbContext) => await AddNewItem(body, dbContext)).RequireCors("allowAny");
         this.app.MapDelete("api/subjectcards/{id}", async (int id, StudyControlDbContext dbContext) =>
         {
@@ -32,6 +34,21 @@ public class SubjectCardEndpointDefinition : IEndpointDefinition
     }
 
     public void DefineServices(IServiceCollection services) {}
+
+    private async Task<IResult> GetItem(StudyControlDbContext dbContext, int subjectCardId)
+    {
+        var subjectCard = await dbContext.SubjectCards
+            .Include(x => x.Controls)
+            .ThenInclude(x => x.ControlTasks)
+            .SingleOrDefaultAsync(x => x.SubjectCardId == subjectCardId);
+
+        if(subjectCard is null)
+        {
+            return Results.NotFound();
+        }
+
+        return Results.Json(subjectCard, new JsonSerializerOptions(JsonSerializerDefaults.Web) { ReferenceHandler = ReferenceHandler.IgnoreCycles });
+    }
 
     private async Task<List<SubjectCard>> GetAllItems(StudyControlDbContext dbContext)
     {
