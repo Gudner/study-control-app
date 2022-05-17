13a9da2c0edc62e63e50640705d4e6d3d4da6ec9
[EgorovMihail] <[EgorovMihail>
2022-05-17 22:50:18 +0300

    Добавил удаление в стейт

--- Frontend/study-control-app/components/addTasckModal/AddTasckModal.jsx
+++ Frontend/study-control-app/components/addTasckModal/AddTasckModal.jsx
@@ -9,122 +9,97 @@ import style from "./AddTasckModal.module.scss";
 import { toJS } from "mobx";
 
 export default observer(function AddTasckModal(props) {
-    let controlItem = props.controlItem; //вот это нужно
-    console.log("controlItem: ", controlItem);
+  let controlItem = props.controlItem;
 
-    // let controlItemControlTasks = props.controlItem;
-    // console.log("controlItemControlTasks: ", controlItemControlTasks);
-    let setControlItem = props.setControlItem;
-    console.log("setControlItem: ", setControlItem);
-    let idControl = props.idControl;
-    console.log("idControl: ", idControl);
+  let setControlItem = props.setControlItem;
+  let idControl = props.idControl;
 
-    let setControlTasks = props.setControlTasks;
-    console.log("setControlTasks: ", setControlTasks);
-    let controlTasks = props.controlTasks; // меняем вот тут данные и запихиваем в массив control item с index idcontrol in mass
-    console.log("controlTasks: ", controlTasks);
+  let setControlTasks = props.setControlTasks;
 
-    let controlTasksMass = controlTasks[0].controlTasks; //то что было
-    console.log("controlTasksMass: ", controlTasksMass);
+  let controlTasks = props.controlTasks;
 
-    let idControlInMass = props.idControlInMass;
-    console.log("idControlInMass: ", idControlInMass);
-    let newControlItem = controlItem;
+  let controlTasksMass = controlTasks[0].controlTasks;
 
-    const ref = useRef();
+  let idControlInMass = props.idControlInMass;
+  let newControlItem = controlItem;
 
-    useEffect(() => {
-        const onClick = (e) =>
-            ref.current.contains(e.target) || stateFormAdd.removeFlagForm();
-        document.addEventListener("click", onClick);
-        return () => {
-            document.removeEventListener("click", onClick);
-        };
-    }, []);
+  const ref = useRef();
 
-    // useEffect(() => {
-    //     console.log(12334);
-    //     setControlItem((сontrolItem) => newControlItem);
-    //     return () => {};
-    // });
-
-    // ====== валидация  для 1 шага=====
-    const validateAddTasck = Yup.object().shape({
-        taskText: Yup.string()
-            .min(2, "Слишком короткое название!")
-            .max(50, "Слишком длинное название!")
-            .required("Это поле является обязателным!"),
-    });
-
-    const submit = (values) => {
-        stateFormAddSubject.removeFlagForm();
-        let obj = {
-            controlId: idControl,
-            taskText: values.taskText,
-        };
-        registrForm(obj);
+  useEffect(() => {
+    const onClick = (e) =>
+      ref.current.contains(e.target) || stateFormAdd.removeFlagForm();
+    document.addEventListener("click", onClick);
+    return () => {
+      document.removeEventListener("click", onClick);
     };
+  }, []);
 
-    //регистрация формы
-    const registrForm = async (values) => {
-        console.log("values", values);
+  // ====== валидация  для 1 шага=====
+  const validateAddTasck = Yup.object().shape({
+    taskText: Yup.string()
+      .min(2, "Слишком короткое название!")
+      .max(50, "Слишком длинное название!")
+      .required("Это поле является обязателным!"),
+  });
 
-        const res = await fetch(
-            "https://backend.revenant-games.online/api/controltasks",
-            {
-                headers: { "Content-Type": "application/json" },
-                method: "PUT",
-                body: JSON.stringify(values, null, 2),
-            }
-        );
-
-        const respons = await res.json();
-        if (res.ok) {
-            
-            console.log("respons from add task modal", respons);
-            let newData = controlTasks[0].controlTasks;
-            console.log("newData: ", newData);
-
-            let newTasck = [...controlTasksMass, respons];
-            console.log("newTasck: ", newTasck);
-            controlTasks[0].controlTasks = newTasck;
-            console.log("controlTasks[0]: ", controlTasks[0]);
-            controlItem[idControlInMass] = controlTasks[0];
-
-            newControlItem = [...controlItem];
-            console.log("controlItem: ", controlItem);
-
-            //setControlItem(newControlItem=>newControlItem);
-             setControlItem(newControlItem);
-
-            /*вот тут обнавляем стейт */
-        }
+  const submit = (values) => {
+    stateFormAddSubject.removeFlagForm();
+    let obj = {
+      controlId: idControl,
+      taskText: values.taskText,
     };
+    registrForm(obj);
+  };
 
-    return (
-        <div className={style.container}>
-            <Formik
-                initialValues={{ taskText: "" }}
-                onSubmit={(values) => submit(values)}
-                validationSchema={validateAddTasck}
-            >
-                {(formik) => (
-                    <Form className={style.form} ref={ref}>
-                        <h3 className={style.formHeader}>Добавление задания</h3>
-                        <div className={style.inputWrap}>
-                            <InputText
-                                id="taskText"
-                                type="text"
-                                placeholder="Текст задания"
-                                name="taskText"
-                            />
-                        </div>
-                        <button type="submit" className={style.addSubjecButton}>
-                            Добавить
-                        </button>
-                    </Form>
-                )}
-            </Formik>
-        </div>
+  //регистрация формы
+  const registrForm = async (values) => {
+    console.log("values", values);
+
+    const res = await fetch(
+      "https://backend.revenant-games.online/api/controltasks",
+      {
+        headers: { "Content-Type": "application/json" },
+        method: "PUT",
+        body: JSON.stringify(values, null, 2),
+      },
     );
+
+    const respons = await res.json();
+    if (res.ok) {
+      let newData = controlTasks[0].controlTasks;
+      let newTasck = [...controlTasksMass, respons];
+      controlTasks[0].controlTasks = newTasck;
+      controlItem[idControlInMass] = controlTasks[0];
+      newControlItem = [...controlItem];
+
+      setControlItem(newControlItem);
+    }
+  };
+
+  return (
+    <div className={style.container}>
+      <Formik
+        initialValues={{ taskText: "" }}
+        onSubmit={(values) => submit(values)}
+        validationSchema={validateAddTasck}
+      >
+        {(formik) => (
+          <Form className={style.form} ref={ref}>
+            <h3 className={style.formHeader}>Добавление задания</h3>
+            <div className={style.inputWrap}>
+              <InputText
+                id="taskText"
+                type="text"
+                placeholder="Текст задания"
+                name="taskText"
+              />
+            </div>
+            <button type="submit" className={style.addSubjecButton}>
+              Добавить
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 });
